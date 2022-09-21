18c4fc88bfc67801c5d574f9fc054f0a36fc397c
Eric Greene <eric@t4d.io>
2022-09-21 15:43:10 -0400

    add exercise 10

---
+++ ToolsApp/ToolsApp.Components/Validators/HexColorValidator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ToolsApp.Components.Validators
+{
+  public class HexColorValidator: ValidationAttribute
+  {
+    private Regex _hexColorRegex;
+
+    public HexColorValidator(int hexColorLength)
+    {
+      _hexColorRegex = new Regex("^[0-9a-fA-F]{" + hexColorLength + "}$");
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+
+      var inputValue = Convert.ToString(value);
+
+      if (inputValue is null)
+      {
+        return new ValidationResult($"{validationContext.DisplayName} is not a valid hex color");
+      }
+
+      if (!_hexColorRegex.IsMatch(inputValue))
+      {
+        return new ValidationResult($"{validationContext.DisplayName} is not a valid hex color");
+      }
+
+      return ValidationResult.Success;
+
+    }
+  }
+}
---
+++ exercises/exercise-10.md
@@ -0,0 +1,15 @@
+# Exercise 10
+
+**Instructions:** Please complete the following requirements ensuring your solution meets all of them. Going above and beyond the requirements is great so long as all of the requirements are met. Before coding anything, please read all of the requirements first. If you are unsure about anything, please ask the instructor.
+
+## Requirements
+
+**Requirement 1.** Upgrade the Car Form component to Edit Form and validations provided by the form model.
+
+**Requirement 2.** On the form model, make all fields required.
+
+**Requirement 3.** Create a custom validator which requires the Car Year to be 1886 or later.
+
+**Requirement 4.** Using the Validation Summary and Validation Message components, please display validation message at the top of the form and for each form control.
+
+Ensure it works!
